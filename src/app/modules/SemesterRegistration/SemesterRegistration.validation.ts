c6cee6116e62f400e83efa87b07d5a9f0be68bf4
aremon-coderower <aremon.rahman@coderower.com>
2024-12-27 08:28:34 +0600

    add checks in offered course section

--- src/app/modules/SemesterRegistration/SemesterRegistration.validation.ts
+++ src/app/modules/SemesterRegistration/SemesterRegistration.validation.ts
@@ -2,20 +2,10 @@ import { z } from 'zod';
 
 const createSemesterRegistrationZodValidation = z.object({
   body: z.object({
-    firstName: z.string({ required_error: 'First name is required' }),
-    lastName: z.string({ required_error: 'Last name is required' }),
-    middleName: z.string().optional(),
-    profileImage: z.string({ required_error: 'Profile image is required' }),
-    email: z.string({ required_error: 'Email is required' }),
-    contactNo: z.string({ required_error: 'Contact no is required' }),
-    gender: z.string({ required_error: 'Gender is required' }),
-    bloodGroup: z.string({ required_error: 'Blood groups is required' }),
-    designation: z.string({ required_error: 'Designation is required' }),
-    academicDepartmentId: z.string({
-      required_error: 'Academic department id is required',
-    }),
-    academicSemesterRegistrationId: z.string({
-      required_error: 'Academic SemesterRegistration id is required',
+    startDate: z.string({ required_error: 'Start date must be a valid date.' }),
+    endDate: z.string({ required_error: 'End date must be a valid date.' }),
+    academicSemesterId: z.string({
+      required_error: 'Academic Semester id is required.',
     }),
   }),
 });
